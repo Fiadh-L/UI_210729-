ebcf0bdea45d5976b1790d24834db13cce3fd28e
sara lim <wooyeon0717@gmail.com>
2021-08-19 00:41:50 +0900

    210818 test

--- css3_0803/WebContent/exam/validation.js
+++ css3_0803/WebContent/exam/validation.js
@@ -3,6 +3,21 @@ window.addEventListener('load', function(){
 				alert('load 성공');
 
 				clearMessage();
+				
+				var formElem=document.querySelector('form');
+				formElem.onsubmit=submitForm;
+
+				//변수방에 불러서 메서드에 저장하는 것이 아니라 이벤트처리하는 것이기 때문에 메서드에 ()를 붙이지 않는다.
+				//월추가
+				var selectInput=document.querySelector('select[name="birth-month"]');
+				for(var i=1; i<=12; i++){
+					var option=document.createElement('option');
+					option.innerHTML=i+'월';
+					option.value=i;
+					
+					selectInput.appendChild(option);
+				}
+				
 });
 
 function clearMessage(){
@@ -20,4 +35,55 @@ function showMessage(inputElement, message){
 	messageElem.innerText=message;
 	
 	inputElement.focus();	
+}
+
+function submitForm(){
+	//account info
+	var accountInput=document.querySelector('input[name="account"]');
+	var passwordInput=document.querySelector('input[name="password"]');
+	var passwordConfirmInput=document.querySelector('input[name="password2"]');
+	var selectInput=document.querySelector('select[name="birth-month"]');
+	var radioInput=document.querySelector('input[name="gender"]:checked');
+	var checkInput=document.querySelector('input[name="agree"]');
+	console.log(accountInput.value);
+	console.log(passwordInput.value);
+	console.log(passwordConfirmInput.value);
+	
+	console.log(selectInput.value);
+	console.log(radioInput.value);
+	console.log(checkInput.value);
+	
+	var success=true;
+	if(accountInput.value.length<6) {
+		showMessage(accountInput, '계정은 6글자 이상이어야 합니다');
+		success=false;
+	}
+	if(passwordInput.value.length<8){
+		showMessage(passwordInput, '비밀번호는 8글자 이상이어야 합니다');
+		success=false;
+	}
+	if(passwordConfirmInput.value!=passwordInput.value){
+		showMessage(passwordConfirmInput, '비밀번호가 일치하지 않습니다');
+		success=false;
+	}
+	
+	return success;
+	
+	/*var accountInput=document.querySelector('input[name="account"]');
+	var passwordInput=document.querySelector('input[name="password"]');
+	var passwordConfirmInput=document.querySelector('input[name="password2"]');
+	
+	//select, radio, checkbox
+	var selectInput=document.querySelector('select[name="birth-month"]');
+	var radioInput=document.querySelector('input[name="gender"]:checked');
+	var checkInput=document.querySelector('input[name="agree"]');
+	
+	console.log(accountInput.value);
+	console.log(passwordInput.value);
+	console.log(passwordConfirmInput.value);
+	console.log(selectInput.value);
+	console.log(radioInput.value);
+	console.log(checkInput.value);
+	
+	return false;*/
 }
